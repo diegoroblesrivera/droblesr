9313c6f1ace04dab5bb9706fd899c70776648d68
Diegorr <diegoroblesrivera@gmail.com>
2025-04-16 16:40:43 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,60 @@
+# nombre="Diego"
+# edad=64
+
+# print("hola", nombre, "su edad es", edad)
+
+# # solicitud de datos
+# print("Ingrese su nombre")
+# nombre=input()
+# print("Ingrese su edad")
+# edad=input()
+# # Ejemplo de concatenacion
+# print("hola", nombre, "su edad es", edad)
+
+# #suma de 2 numeros
+# print("ingrese 2 numeros")
+# n1=int(input())
+# n2=int(input())
+
+# print("La suma de los numeros es", n1+n2)
+
+# #ingresar 3 numeros y sacar el promedio
+# print("ingrese 2 numeros")
+# n1=int(input())
+# n2=int(input())
+# n3=int(input())
+# prom=(n1+n2+n3)/3
+# print("El promedio  es", prom )
+
+# if prom>=40:
+#     print("el alumno aprobó")    
+# else:
+#     print("el alumno reprobó")
+
+#pedir edad al usuario y determinar si es mayor de edad
+
+# edad=int(input("Ingrese su edad "))
+
+# if edad>=18:
+#     print("Usted es mayor de edad")
+# else:
+#     print("Usted NO es mayor de edad")
+
+
+# # mostrar segun criterio
+# # menos de 12 años es niños
+# # entre 12 y 17 es adolescente
+# # entre 18 y 64 es adulto
+# # 65 y mas , es adulto mayor
+# edad=int(input("Ingrese su edad "))
+
+# if edad<12:
+#     print("Usted es un niño")
+# elif edad>=12 and edad<18:          # else if
+#     print("Usted es adolescente")
+# elif edad>=18 and edad<65: 
+#     print("Usted es adulto")
+# else:
+#     print("Usted es un tatita")
+
+#ingresar 3 numeros y mostrar el mayor de los 3
