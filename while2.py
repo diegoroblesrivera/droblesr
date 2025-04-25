217d874d36290a8777c5b2dcbc81c56ad4e27be9
Diegorr <diegoroblesrivera>
2025-04-25 18:50:27 -0400

    johjojo

--- while2.py
+++ while2.py
@@ -4,6 +4,8 @@
 # while num<=5:
 #     print(num)
 #     num+=1
+
+
 # import time
 # num= 10
 
@@ -17,11 +19,67 @@
 # while resp!="si":
 #     resp=input("Desea Salir del sistema? :")
 
-clave=3344
+# clave=3344
+# intentos=3
+# contraseña=int(input("Ingrese su contraseña"))
 
-contraseña=int(input("Ingrese su contraseña"))
+# while clave!=contraseña :
+#     intentos-=1
+#     print("Quedan ", intentos, " intentos")
+#     print("ERROR; clave invalida")
+#     contraseña=int(input("Ingrese su contraseña"))
+#     if intentos==1:
+#         break
 
-while clave!=contraseña:
-    print("ERROR; clave invalida")
-    contraseña=int(input("Ingrese su contraseña"))
-print(" Clave aceptada")
+
+# if clave==contraseña:
+#     print(" Clave aceptada")
+# else:
+#     print("Sistema bloqueado")
+
+
+
+# pida al usuaruio una palbra y cuentes sus caracteres
+
+# palabra=input("ingrese una plabra ")
+
+
+
+# # caract=0
+# # for i in palabra:
+# #     print(i)
+# #     # caract=caract+1
+# #     caract+=1
+# print("La cant de caracteres es" , len(palabra))
+
+
+# pida al usuario numero infinitamente y 
+# muestre si son par o impar
+# para salir, ponga 0 (cero).
+# num=5
+
+# while num!=0:
+#     num=int(input("Ingrese un numero( cero para salir)"))
+#     if num % 2==0:
+#         print(f"El numero {num} es par")
+#     else:
+#         print(f"El numero {num} es impar")
+
+# ingresar numeros hasta que ponga el cero
+# mostrar las suma de todos los numeros 
+# pares e impares ingresados
+
+# print("/"*100)
+
+import random
+randy=random.randint(1,10)
+print(randy)
+# STREET FIGTHER
+
+# designe 2 peleadores cada uno con 50 HP
+# peleas por turnos, casa uno acata entre 2 y 10
+# gana quien quita todo el HP al contrincante
+
+
+
+
