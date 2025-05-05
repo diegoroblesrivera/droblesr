130f556ba83710832d18a103dee2fdd5bd57dd04
Diegorr <diegoroblesrivera@gmail.com>
2025-05-05 11:17:38 -0400

    materias

--- 001D/if.py
+++ 001D/if.py
@@ -112,3 +112,39 @@ print("▄"*num)
 # 4.5 y 5: 300, 5.1 y 6.0: 500, 6.1 y 7.0 : 800
 # Agregar puntaje segun carrera
 # Tecnico :+60, Ingenieria: +40, Diplomado: +20
+
+mat=int(input("Ingrese la cant de materias: "))
+suma=0
+for i in range(mat):
+   notaramo=float(input(f"Ingrese la nota del ramo{i+1}: "))
+   suma+=notaramo
+prom=suma/mat
+print("Su nota final es ", round(prom,1))
+if prom>=4.5 and prom<=5.0:
+   puntaje=300
+   print( f"Su puntaje de beneficios es de {puntaje}")
+elif prom>=5.2 and prom<=6.0:
+    puntaje=500
+    print( f"Su puntaje de beneficios es de {puntaje}")
+elif prom>=6.1 and prom<=7.0:
+    puntaje=800
+    print( f"Su puntaje de beneficios es de {puntaje}")
+else:
+   print(" Es tan porro que no tiene beneficios")
+
+car=int(input('''
+            Ingrese su tipo de grado
+              1.- Tecnico
+              2.- Ingenieria
+              3.- Diplomado
+              '''))
+if car==1:
+   puntaje+=60
+elif car==2:
+   puntaje+=40
+elif car==3:
+   puntaje+=20
+else:
+   print("no es nuemro valido")
+
+print( " El puntaje de beneficios es ", puntaje)
