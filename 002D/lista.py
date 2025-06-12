5763f529e7bd82762657fa83daae094c26ee866b
Diegorr <diegoroblesrivera@gmail.com>
2025-06-12 15:30:19 -0400

    Diccinnariosd

---
+++ 002D/lista.py
@@ -0,0 +1,83 @@
+# uso y ejemplo de listas
+
+#        -5 -4 -3  -2  -1
+numeros=[10, 5, 9 , 34, 20]
+#         0  1  2   3   4
+
+print(numeros[-1])
+# numeros.reverse()
+# for numero in numeros:
+#     print("numero" ,numero*3)
+# numeros.append(64)
+# print(numeros)
+
+# frutas=["Durazno", "Naranja", "Manzana", "Pera"]
+
+# for fruta in frutas:
+#     print(fruta)
+
+# nombres=["Steven","George"]
+# appellidos=["Spilvergo", "Lucrecia"]
+#  while True:
+#     print('''
+#         1.- Ingresa un nombre y apellido
+#         2.- Buscar Nombre
+#         3.- Mostrar nombres y apellidos
+#         4.- Salir
+#           ''')
+#     op=int(input("seleccione una opcion"))
+#     match op:
+#         case 1:
+#             persona=input("Ingrese un nombre")
+#             nombres.append(persona)
+#             ape=input("Ingrese un apellido")
+#             appellidos.append(ape)
+#             print(nombres)
+#             print(len(nombres))
+#         case 2:
+#             n=input("ingrese nombre a buscar")
+#             if n in nombres:
+#                 print(f"El nombre {n} Existe")
+#             else:    
+#                 print(f"El nombre {n} NO Existe")
+#         case 3:
+#             # c=0
+#             # for karina in nombres:
+#             #     print(nombres[c], appellidos[c])
+#             #     c+=1
+#             for i in range(len(nombres)):
+#                 print(nombres[i], appellidos[i])
+#         case 4:
+#             print("Saliendo")
+#             break
+#         case _:
+#             print("Opcion invalida")
+
+'''
+Selecciones una opcion
+1.- Agregar Productos (nombre del producto y precio)
+2.- Comprar (submenu mostrando productos y precios)
+3.- Crear Boleta
+4.- Salir
+productos=[]
+precios=[]
+carrito[]
+
+
+'''
+
+
+
+
+
+
+'''
+Tarea
+Crear programa de manejo de notas
+1.- Ingresar nota
+2.- Borrar nota
+3.- Mostar notas
+4.- Sacar promedio, nota mayor y nota menor
+5.- Limpiar todas las notas
+6.- Salir
+'''
