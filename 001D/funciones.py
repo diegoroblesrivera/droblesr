32a126ae59f4e72e5c4df7ba7a76aff61bdb131c
Diegorr <diegoroblesrivera@gmail.com>
2025-06-18 17:24:26 -0400

    Funciones dic

--- 001D/funciones.py
+++ 001D/funciones.py
@@ -1,4 +1,4 @@
-https://github.com/diegoroblesrivera/droblesr
+# https://github.com/diegoroblesrivera/droblesr
 
 # uso y ejemplos de funciones
 
@@ -68,67 +68,153 @@ Hacer otro para calcular el IVA
 # productos=["Zapato"]
 # precio=[20000]
 
-list_prod=[
+
+
+# print(prod_sport[0]["nombre"])
+
+# print(prod_sport)
+
+# # prod_sport.insert(0,{"nombre":"paleta", "precio":14000})
+
+# print(prod_sport)
+
+prod_college=[
+    {"nombre":"lapiz", 
+     "precio":400},
+
+    {"nombre":"goma", 
+     "precio":300} ,
+     
+    {"nombre":"estuche", 
+     "precio":2000} 
+]
+prod_sport=[
     {"nombre":"zapato", 
-     "precio":20000, 
-     "categoria": "vestuario",
-     "tamaño": "mediano",
-     "enOferta": False},
+     "precio":20000},
 
     {"nombre":"pelota", 
-     "precio":24000}
+     "precio":24000} ,
+     
+    {"nombre":"raqueta", 
+     "precio":74000} 
 ]
+prod_sport[0]["nombre"]
 
-# print(list_prod[0]["nombre"])
+def mostrar_art(lista):
+    for n, p in enumerate(lista):
+        print(n+1, ".- ", p["nombre"], "$", p["precio"] )
+def insertar(lista):
+    nom=input("Ingrese el nombre del producto: ")
+    pre=int(input("Ingrese el precio: "))
+    lista.insert(0,{"nombre":nom, "precio":pre})
+def actualizar(lista):
+    mostrar_art(lista) 
+    opc=int(input("Seleccione el producto a actualizar"))
+    print(lista[opc-1])
+    nn=input("Ingrese nuevo Nombre")
+    np=int(input("Ingrese nuevo Precio"))
+    lista[opc-1]["nombre"]=nn
+    lista[opc-1]["precio"]=np
+    print("Articulo actualizado!")
+def borrar(lista):
+    mostrar_art(lista) 
+    opc=int(input("Seleccione el producto a borrar"))
+    lista.pop(opc-1)
+def menu(lista):
+    while True:
+        try:
+            print('''
+                1.- Agregar producto
+                2.- Mostrar Productos
+                3.- Actualizar producto
+                4.- Borrar Producto
+                5.- Salir
+                ''')
+            op=int(input("Seleccione una opcion: "))
+            match op:
+                case 1:
+                    insertar(lista)
+                case 2:
+                    mostrar_art(lista)      
+                case 3:
+                    actualizar(lista)
+                case 4:
+                    borrar(lista)
+                case 5:
+                    break
+                case _:
+                    print("Opcion invalida")
+        except Exception:
+            print("Solo numeros enteros")
 
-# print(list_prod)
-
-# # list_prod.insert(0,{"nombre":"paleta", "precio":14000})
-
-# print(list_prod)
+# menu(prod_college)
 
 
+'''
+crear programa CRUD del siguiente 
+diccionario
+'''
+
+personas={
+    1:{"nombre": " Diego Rivera",
+       "numeros":[7565434,97834231],
+       "estadoCivil": "Casado",
+       "trabajando": True,
+       "edad": 64},
+    2:{"nombre": " Diego Rivera",
+       "numeros":[7565434,97834231],
+       "estadoCivil": "Casado",
+       "trabajando": True,
+       "edad": 64},
+    3:{"nombre": " Diego Rivera",
+       "numeros":[7565434,97834231],
+       "estadoCivil": "Casado",
+       "trabajando": True,
+       "edad": 64}
+}
 
 
 while True:
     try:
         print('''
-            1.- Agregar producto
-            2.- Mostrar Productos
-            3.- Actualizar producto
-            4.- Borrar Producto
-            5.- Salir
-            ''')
-        op=int(input("Seleccione una opcion: "))
+        1.- Ingresar Persona
+        2.- Mostrar listado
+        3.- Actualizar persona
+        4.- Borrar persona
+        5.- Salir
+        ''')
+        op=int(input("Seleccione una opcion"))
         match op:
             case 1:
-                nom=input("Ingrese el nombre del producto: ")
-                pre=int(input("Ingrese el precio: "))
-                list_prod.insert(0,{"nombre":nom, "precio":pre})
+                nombre=input("Ingrese el nombre: ")
+                numero=int(input("Ingrese el numero: "))
+                est=int(input("estado Civil 1.- Casado, 2.- Soltero: "))
+                if est==1:
+                    estCivil="Casado"
+                else:
+                    estCivil="Soltero"
+                edad=int(input("Ingrese la edad: "))
+                nextkey=len(personas)
+                personas[nextkey+1]={"nombre": nombre,
+                "numeros":[numero],
+                "estadoCivil": estCivil,
+                "trabajando": True,
+                "edad": edad}
+                print("Persona ingresada con exito")
             case 2:
-                for p in list_prod:
-                    print(p)        
-            case 3:
-                # for n, p in enumerate(list_prod):
-                #     print(n+1, ".- ", p)
-                for i in range(len(list_prod)):
-                    print(i+1, ".-", list_prod[i])
-                opc=int(input("Seleccione el producto a actualizar"))
-                print(list_prod[opc-1])
-                nn=input("Ingrese nuevo Nombre")
-                np=int(input("Ingrese nuevo Precio"))
-                list_prod[opc-1]["nombre"]=nn
-                list_prod[opc-1]["precio"]=np
-                print("Articulo actualizado!")
+                for persona, val in personas.items():
+                    print(persona, val)
+            case 4:
+                for persona, val in personas.items():
+                    print(persona, val)
+                dell=int(input("Seleccione cual desea borrar"))
+                del personas[dell]
             case 5:
                 break
             case _:
                 print("Opcion invalida")
-    except Exception:
-        print("Solo numeros enteros")
-
-
-
+    except Exception as e:
+        print("EL error es: ", e)
 
 
 
